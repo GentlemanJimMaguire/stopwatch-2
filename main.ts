06b8c9e9941b0e37224a33ebad9e1fa5afc891d6
GentlemanJimMaguire <124887271+GentlemanJimMaguire@users.noreply.github.com>
2023-03-02 11:27:44 +0000

    Restore '02/03/2023, 09:55:46 Update main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,37 +1,12 @@
-let i = 0
+let start = 0
+let elapsed = 0
 input.onButtonPressed(Button.A, function () {
-    i = 0
+    start = input.runningTime()
 })
-function pause2 () {
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . # . .
-        . . . . .
-        . . . . .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . # # # .
-        . # . # .
-        . # # # .
-        . . . . .
-        `)
-    basic.showLeds(`
-        # # # # #
-        # . . . #
-        # . . . #
-        # . . . #
-        # # # # #
-        `)
-}
-input.onButtonPressed(Button.AB, function () {
-    basic.showNumber(i)
-    pause2()
-    basic.showNumber(Math.idiv(i, 3))
-    pause2()
-    basic.showNumber(Math.round(i / 3))
-    pause2()
-    basic.showNumber(Math.floor(i / 3))
-    i += 1
+input.onButtonPressed(Button.B, function () {
+    elapsed = input.runningTime() - start
+    basic.showNumber(Math.idiv(elapsed, 1000))
+})
+basic.forever(function () {
+	
 })
