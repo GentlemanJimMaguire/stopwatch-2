61a622504398054fdec78f8e9366ea289ca9a54b
GentlemanJimMaguire <124887271+GentlemanJimMaguire@users.noreply.github.com>
2023-03-02 11:30:51 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,38 @@
-let start = 0
-let elapsed = 0
 input.onButtonPressed(Button.A, function () {
-    start = input.runningTime()
+    i = 0
 })
+function pause2 () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # . # .
+        . # # # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+}
 input.onButtonPressed(Button.B, function () {
-    elapsed = input.runningTime() - start
-    basic.showNumber(Math.idiv(elapsed, 1000))
+    basic.showNumber(i)
+    pause2()
+    basic.showNumber(Math.idiv(i, 3))
+    pause2()
+    basic.showNumber(Math.round(i / 3))
+    pause2()
+    basic.showNumber(Math.floor(i / 3))
+    i += 1
 })
-basic.forever(function () {
-	
-})
+let i = 0
+i = 0
